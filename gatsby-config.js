e8bf29ed152fa42d05cec9438a0dcebc91a6e53c
Arieseco <fusianasan.jimso@gmail.com>
2021-09-27 23:49:29 +0900

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,11 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://build-6b0a5894-6f88-4414-b45e-14879ea1a52c.gtsb.io/",
     title: "My Gatsby Site",
   },
   plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
     "gatsby-plugin-mdx",
     {
       resolve: "gatsby-source-filesystem",
