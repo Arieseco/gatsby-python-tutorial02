498578d3023852eea3bd72e600a8d807a4af1518
Arieseco <fusianasan.jimso@gmail.com>
2021-09-28 00:31:59 +0900

    mdページ遷移追加

---
+++ gatsby-node.js
@@ -0,0 +1,62 @@
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const blogPost = path.resolve(`./src/pages/blog/blog-post.jsx`)
+  const result = await graphql(
+    `
+      {
+        allMdx(
+          sort: { fields: [frontmatter___date], order: DESC }
+          limit: 1000
+        ) {
+          nodes {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+            }
+          }
+        }
+      }
+    `
+  )
+
+  if (result.errors) {
+    throw result.errors
+  }
+
+  // Create blog posts pages.
+  const posts = result.data.allMdx.nodes
+
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1]
+    const next = index === 0 ? null : posts[index - 1]
+
+    createPage({
+      path: `/blog${post.fields.slug}`,
+      component: blogPost,
+      context: {
+        slug: post.fields.slug,
+        previous,
+        next,
+      },
+    })
+  })
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  
+  if (node.internal.type === `Mdx`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
---
+++ src/pages/blog/blog-post.jsx
@@ -0,0 +1,72 @@
+import * as React from 'react'
+import { graphql,Link } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+import Layout from '../../components/layout'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+
+const BlogPost = ({data, pageContext}) => {
+  const { previous, next } = pageContext
+  console.log(previous)
+  console.log(next)
+  console.log(pageContext)
+  const image = getImage(data.mdx.frontmatter.hero_image) 
+  return (
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage
+      image={image}
+      alt={data.mdx.frontmatter.hero_image_alt}/>
+      <p>
+        Photo Credit:{" "}
+        <Link to={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </Link>
+      </p>
+      <MDXRenderer>
+        {data.mdx.body}
+      </MDXRenderer>
+      <nav className="page-links">
+        <h2>More Readings</h2>
+        {!previous && !next && <p>Nothing here yet!</p>}
+        <ul>
+          <li>
+            {previous && (
+              <Link to={`/blog${previous.fields.slug}`} rel="prev">
+                {previous.frontmatter.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={`/blog${next.fields.slug}`} rel="next">
+                {next.frontmatter.title}
+              </Link>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+query($slug: String!) {
+  mdx(fields: { slug: { eq: $slug } }) {
+    body
+    frontmatter {
+      date(formatString: "MMMM DD, YYYY")
+      title
+      hero_image_alt
+      hero_image_credit_link
+      hero_image_credit_text
+      hero_image {
+        childImageSharp {
+          gatsbyImageData
+        }
+      }
+    }
+  }
+}
+`
+
+export default BlogPost
